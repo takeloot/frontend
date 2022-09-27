39aec2978490623fc71beb46cb5e3d7e96d9b37b
kislyj <kirillkislyj2011@yandex.ru>
2022-09-27 21:47:54 +0300

    Standart title

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React, {FC, ReactNode} from "react";
 
 import {Footer} from "_components/Footer";
@@ -7,11 +8,17 @@ import {Navbar} from "_app/components";
 
 interface IProps {
   children: ReactNode;
+  title?: string;
 }
 
-export const MainLayout: FC<IProps> = ({children}) => {
+export const MainLayout: FC<IProps> = ({children, title}) => {
   return (
     <Box bg="gray.800" h="calc(100%)">
+      <Head>
+        <title>{title || "takeloot - Продать скины CS:GO за реальные деньги с моментальным выводом"}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
       {children}
       <Footer />
