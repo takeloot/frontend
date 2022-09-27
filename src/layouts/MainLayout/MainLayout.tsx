e3f45e4629a6f06ec84088a7024e34a4a6d47f85
kislyj <kirillkislyj2011@yandex.ru>
2022-09-27 21:52:03 +0300

    Standart title

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
 import React, {FC, ReactNode} from "react";
 
+import Head from "next/head";
 import {Footer} from "_components/Footer";
 import {Box} from "@chakra-ui/react";
 
@@ -13,15 +13,17 @@ interface IProps {
 
 export const MainLayout: FC<IProps> = ({children, title}) => {
   return (
-    <Box bg="gray.800" h="calc(100%)">
+    <>
       <Head>
         <title>{title || "takeloot - Продать скины CS:GO за реальные деньги с моментальным выводом"}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Navbar />
-      {children}
-      <Footer />
-    </Box>
+      <Box bg="gray.800" h="calc(100%)">
+        <Navbar />
+        {children}
+        <Footer />
+      </Box>
+    </>
   );
 };
