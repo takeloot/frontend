a268a08ddf835416c5509a5adaf5262f1674c1a0
Aleksey Tsvetkov <code.aleksey@icloud.com>
2022-10-08 02:19:01 +0300

    updated

--- src/components/user-panel/user-skeleton.tsx
+++ src/components/user-panel/user-skeleton.tsx
@@ -2,7 +2,7 @@ import {Menu} from "@headlessui/react";
 
 export const UserProfileSkeleton = () => {
   return (
-    <Menu as="div" className="relative max-h-10">
+    <Menu as="div" className="relative pt-2">
       <Menu.Button>
         <div className="h-10 w-10 rounded-full bg-tl-dark-gray" />
       </Menu.Button>
