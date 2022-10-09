7f3acb19a5d02e675818acada647d456381aa2e8
kislyj <kirillkislyj2011@yandex.ru>
2022-10-09 17:48:33 +0300

    Language selector

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
   const userQuery = useMeQuery();
   const {t} = useTranslation("common");
 
-  const [language, setLanguage] = useState(LANGUAGES[0]);
+  const [language, setLanguage] = useState(router.locale);
   const [currency, setCurrency] = useState(CURRENCIES[0]);
   // TODO: Get from data of useOnlineQuery hook when will be done on backend
   const [online, setOnline] = useState(randomInteger(650, 750));
@@ -101,7 +101,7 @@ export const Navbar = () => {
           <Listbox value={language} onChange={setLanguage}>
             <div className="relative mt-1">
               <Listbox.Button className="relative w-full cursor-default rounded-lg bg-surface py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-cloud sm:text-sm">
-                <span className="block truncate">{language.toUpperCase()}</span>
+                <span className="block truncate">{language?.toUpperCase()}</span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                   <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                 </span>
@@ -113,29 +113,32 @@ export const Navbar = () => {
                 leaveTo="opacity-0"
               >
                 <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-surface py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                  {LANGUAGES.map((language, languageIdx) => (
-                    <Listbox.Option
-                      key={languageIdx}
-                      className={({active}) =>
-                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                          active ? "bg-gray text-white" : "text-white"
-                        }`
-                      }
-                      value={language}
-                    >
-                      {({selected}) => (
-                        <>
-                          <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                            {language.toUpperCase()}
-                          </span>
-                          {selected ? (
-                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
-                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                            </span>
-                          ) : null}
-                        </>
-                      )}
-                    </Listbox.Option>
+                  {router.locales?.map((language, languageIdx) => (
+                    <Link href={router.asPath} locale={language} key={languageIdx}>
+                      <a>
+                        <Listbox.Option
+                          className={({active}) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active ? "bg-gray text-white" : "text-white"
+                            }`
+                          }
+                          value={language}
+                        >
+                          {({selected}) => (
+                            <>
+                              <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                                {language.toUpperCase()}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      </a>
+                    </Link>
                   ))}
                 </Listbox.Options>
               </Transition>
