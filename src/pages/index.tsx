eb794815f51618c9ef2272b5456b6f7542468cb0
kislyj <kirillkislyj2011@yandex.ru>
2022-10-09 12:03:17 +0300

    Hello page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,38 @@
+import React from "react";
 import type {GetStaticProps, NextPage} from "next";
 
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {useTranslation} from "next-i18next";
 
 import {MainLayout} from "_app/layouts/main-layout";
+import {useMeQuery} from "_app/generated/graphql";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const {t} = useTranslation("common");
+  const userQuery = useMeQuery();
+
+  const user = userQuery.data?.me;
 
   return (
     <MainLayout>
-      <div className="rounded-lg border border-gray bg-surface p-2">
-        <div className="text-lg">{t("sell_csgo_skins")}</div>
+      <div className="flex flex-col items-center pt-20 text-center align-middle">
+        <div className="m-auto my-2 w-full text-6xl font-extrabold font-black tracking-tight text-gray-200">
+          Sell your CS:GO skins <br /> for the good price.
+        </div>
+        <div className="m-auto my-10 w-full text-2xl text-gray-400">
+          Your best choice to sell your skins <br />
+          fast & safe
+        </div>
+        {!user ? (
+          <Link href="/api/auth/steam?continue=">
+            <div className="w-1/5 rounded-lg bg-gray py-2 px-4 text-lg hover:cursor-pointer">Login with Steam</div>
+          </Link>
+        ) : (
+          <Link href="/sell">
+            <div className="w-1/5 rounded-lg bg-gray py-2 px-4 text-lg hover:cursor-pointer">Start selling</div>
+          </Link>
+        )}
       </div>
     </MainLayout>
   );
