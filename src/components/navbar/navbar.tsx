a578326f34c47389efd5661616d4ada3e04ba9f9
Aleksey Tsvetkov <code.aleksey@icloud.com>
2022-10-13 22:09:17 +0300

    fixed z index

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -112,7 +112,7 @@ export const Navbar = () => {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-surface py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-surface py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                   {router.locales?.map((language, languageIdx) => (
                     <Link href={router.asPath} locale={language} key={languageIdx}>
                       <a>
@@ -160,7 +160,7 @@ export const Navbar = () => {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-surface py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-surface py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                   {CURRENCIES.map((currency, currencyIdx) => (
                     <Listbox.Option
                       key={currencyIdx}
